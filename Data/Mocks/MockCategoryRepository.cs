b85fc55468e3098f868a9ec488582e7cb9e2b810
prorok.rv <prorok.rv@gmail.com>
2020-06-25 11:23:22 +0300

    add mock

---
+++ Data/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_store.Data.Interfaces;
+using web_store.Data.Models;
+
+namespace web_store.Data.mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories
+        {
+            get
+            {
+                return new List<Category>
+                     {
+                        new Category { CategoryName = "Tea", Description = "Non-alcoholic drinks" },
+                         new Category { CategoryName = "Beer", Description = "Alcoholic drinks" },
+                          new Category { CategoryName = "Coffy", Description = "Non-alcoholic drinks" },
+                           new Category { CategoryName = "Dairy products", Description = "Non-alcoholic drinks" },
+                            new Category { CategoryName = "Vodka", Description = "Alcoholic drinks" },
+                             new Category { CategoryName = "Wine", Description = "Alcoholic drinks" },
+                              new Category { CategoryName = "Cognac", Description = "Alcoholic drinks" },
+                               new Category { CategoryName = "Mineral Water", Description = "Non-alcoholic drinks" }
+                     };
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ServiceStack.Text;
+using web_store.Data.Interfaces;
+using web_store.Data.mocks;
+using web_store.Data.Mocks;
 
 namespace web_store
 {
@@ -17,6 +20,8 @@ namespace web_store
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IDrinkRepository, MockDrinkRepository>();
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
             services.AddMvc();
         }
 
