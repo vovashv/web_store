ffee46d1639a1fd7eead852981dd1b88bb59bbb2
prorok.rv <prorok.rv@gmail.com>
2020-08-22 10:34:01 +0300

    add ShoppingCart controller + view

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web_store.Data.Interfaces;
+using web_store.Data.Models;
+using web_store.ViewModels;
+
+namespace web_store.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartController(IDrinkRepository drinkRepository, ShoppingCart shoppingCart)
+        {
+            _drinkRepository = drinkRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var sCVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(sCVM);
+        }
+            public RedirectToActionResult AddToShoppingCart(int drinkId)
+        {
+            var selectedDrink = _drinkRepository.Drinks.FirstOrDefault(p => p.DrinkId == drinkId);
+            if (selectedDrink != null)
+            {
+                _shoppingCart.AddToCart(selectedDrink, 1);
+            }
+            return RedirectToAction("Index");
+        }
+        public RedirectToActionResult RemoveFromShoppingCart(int drinkId)
+        {
+            var selectedDrink = _drinkRepository.Drinks.FirstOrDefault(p => p.DrinkId == drinkId);
+            if (selectedDrink != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedDrink);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_store.Data.Models;
+
+namespace web_store.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
